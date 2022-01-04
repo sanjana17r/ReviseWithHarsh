6169c0e56528107d46505af4b01c6228e928ced1
sanjana17r <38381244+sanjana17r@users.noreply.github.com>
2022-01-05 00:31:09 +0530

    13. Decode String

---
+++ decodeString.py
@@ -0,0 +1,12 @@
+def decodedString(self, s):
+        stk, ans, n = deque(), "", ""
+        for c in s:
+            if c.isalpha():   ans += c
+            elif c.isdigit(): n += c
+            elif c == '[': 
+                stk.append((n, ans))
+                n, ans = "", ""
+            else:
+                cnt, prev = stk.pop()
+                ans = prev + ans * int(cnt)
+        return ans
