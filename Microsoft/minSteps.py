29d02b178750a04d23930b0d1912b7e23dfedc89
sanjana17r <38381244+sanjana17r@users.noreply.github.com>
2022-01-20 19:40:37 +0530

    14. find the minimum number of steps

---
+++ Microsoft/minSteps.py
@@ -0,0 +1,10 @@
+def minSteps(self, target):
+        # code here
+        t = abs(target)
+        n = math.floor(math.sqrt(2*t))
+        while True:
+            to_minus = ((n+1)*n)/2 - t 
+            if to_minus >= 0:  
+                if to_minus%2==0:
+                    return int(n)
+            n+=1
