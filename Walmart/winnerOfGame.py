d8f7b43b38a994f224eda4ae2c789bc931a9ab71
sanjana17r <38381244+sanjana17r@users.noreply.github.com>
2022-01-30 10:37:17 +0530

    3. winnerOfGame

---
+++ Walmart/winnerOfGame.py
@@ -0,0 +1,9 @@
+def winnerOfGame(self, colors: str) -> bool:
+        
+        a,b=0,0
+        for i in range(1,len(colors)-1):
+            if colors[i]=='A' and colors[i-1] == 'A' and colors[i+1] == 'A':
+                a+=1
+            if colors[i]=='B' and colors[i-1] == 'B' and colors[i+1] == 'B':
+                b+=1
+        return a>b
